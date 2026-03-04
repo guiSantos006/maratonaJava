1b8c024d6395c8054ea2ff608284ca52ab03cf9d
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-03-04 12:13:42 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        // Escritas em minúsculo
+
+        int ageFather = 49;
+        System.out.println("The father age is: "+ageFather);
+
+
+    }
+}
