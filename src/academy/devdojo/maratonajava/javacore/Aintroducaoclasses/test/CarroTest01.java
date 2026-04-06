fd36ffc9882d5cb41b16a726b084f55caa9f0cc9
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-04-06 12:07:03 -0300

    Aula metodos com criacao de calculadora

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -12,6 +12,9 @@ public class CarroTest01 {
         carro02.nome = "Jetta";
         carro02.modelo = "2.0";
         carro02.ano = 2020;
+
+        carro01 = carro02;
+
         System.out.println("Carro 1\n" + " Nome: " + carro01.nome + " | Modelo: " + carro01.modelo + " | Ano: " +carro01.ano);
         System.out.println("Carro 2\n" + " Nome: " + carro02.nome + " | Modelo: " + carro02.modelo + " | Ano: " +carro02.ano);
 
