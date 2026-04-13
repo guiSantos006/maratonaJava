e73b2e992cd1ec97321b800cd33bb75027fda51f
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-04-13 12:52:55 -0300

    Metodos Exercise

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome="Charles";
+        funcionario.idade= 29;
+
+        funcionario.imprimeFuncionario();
+    }
+}
